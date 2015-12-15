d197664cfae1acaa03821f865c0df05834011395
wmatilde <william.matilde12@gmail.com>
2015-12-15 16:31:32 +0100

    2ème test Fixe vers Portable Ajout des commentaires

--- TPentrepriseGraphique/Form1.Designer.cs
+++ TPentrepriseGraphique/Form1.Designer.cs
@@ -66,28 +66,29 @@
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 16;
-            this.listBox1.Location = new System.Drawing.Point(15, 29);
+            this.listBox1.Location = new System.Drawing.Point(11, 24);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(254, 116);
+            this.listBox1.Size = new System.Drawing.Size(192, 95);
             this.listBox1.TabIndex = 0;
             this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
             // 
             // listBox2
             // 
             this.listBox2.FormattingEnabled = true;
-            this.listBox2.ItemHeight = 16;
-            this.listBox2.Location = new System.Drawing.Point(15, 168);
+            this.listBox2.Location = new System.Drawing.Point(11, 136);
+            this.listBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.listBox2.Name = "listBox2";
-            this.listBox2.Size = new System.Drawing.Size(254, 116);
+            this.listBox2.Size = new System.Drawing.Size(192, 95);
             this.listBox2.TabIndex = 1;
             this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
             // 
             // btnaddsal
             // 
-            this.btnaddsal.Location = new System.Drawing.Point(193, 290);
+            this.btnaddsal.Location = new System.Drawing.Point(145, 236);
+            this.btnaddsal.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnaddsal.Name = "btnaddsal";
-            this.btnaddsal.Size = new System.Drawing.Size(76, 53);
+            this.btnaddsal.Size = new System.Drawing.Size(57, 43);
             this.btnaddsal.TabIndex = 2;
             this.btnaddsal.Text = "Ajouter";
             this.btnaddsal.UseVisualStyleBackColor = true;
@@ -96,26 +97,29 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(66, 9);
+            this.label1.Location = new System.Drawing.Point(50, 7);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(162, 17);
+            this.label1.Size = new System.Drawing.Size(121, 13);
             this.label1.TabIndex = 3;
             this.label1.Text = "Lise des Départements :";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(79, 148);
+            this.label2.Location = new System.Drawing.Point(59, 120);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(126, 17);
+            this.label2.Size = new System.Drawing.Size(93, 13);
             this.label2.TabIndex = 4;
             this.label2.Text = "Liste des salariés :";
             // 
             // btnsupprsal
             // 
-            this.btnsupprsal.Location = new System.Drawing.Point(95, 290);
+            this.btnsupprsal.Location = new System.Drawing.Point(71, 236);
+            this.btnsupprsal.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnsupprsal.Name = "btnsupprsal";
-            this.btnsupprsal.Size = new System.Drawing.Size(92, 53);
+            this.btnsupprsal.Size = new System.Drawing.Size(69, 43);
             this.btnsupprsal.TabIndex = 5;
             this.btnsupprsal.Text = "Supprimer";
             this.btnsupprsal.UseVisualStyleBackColor = true;
@@ -123,9 +127,10 @@
             // 
             // btndep
             // 
-            this.btndep.Location = new System.Drawing.Point(275, 57);
+            this.btndep.Location = new System.Drawing.Point(206, 46);
+            this.btndep.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btndep.Name = "btndep";
-            this.btndep.Size = new System.Drawing.Size(172, 42);
+            this.btndep.Size = new System.Drawing.Size(129, 34);
             this.btndep.TabIndex = 6;
             this.btndep.Text = "Ajouter un département";
             this.btndep.UseVisualStyleBackColor = true;
@@ -133,16 +138,18 @@
             // 
             // txtdep
             // 
-            this.txtdep.Location = new System.Drawing.Point(318, 29);
+            this.txtdep.Location = new System.Drawing.Point(238, 24);
+            this.txtdep.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtdep.Name = "txtdep";
-            this.txtdep.Size = new System.Drawing.Size(128, 22);
+            this.txtdep.Size = new System.Drawing.Size(97, 20);
             this.txtdep.TabIndex = 7;
             // 
             // supprdep
             // 
-            this.supprdep.Location = new System.Drawing.Point(275, 105);
+            this.supprdep.Location = new System.Drawing.Point(206, 85);
+            this.supprdep.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.supprdep.Name = "supprdep";
-            this.supprdep.Size = new System.Drawing.Size(171, 40);
+            this.supprdep.Size = new System.Drawing.Size(128, 32);
             this.supprdep.TabIndex = 14;
             this.supprdep.Text = "Supprimer département";
             this.supprdep.UseVisualStyleBackColor = true;
@@ -151,17 +158,19 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(275, 32);
+            this.label6.Location = new System.Drawing.Point(206, 26);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(37, 17);
+            this.label6.Size = new System.Drawing.Size(29, 13);
             this.label6.TabIndex = 15;
             this.label6.Text = "Nom";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(15, 290);
+            this.button1.Location = new System.Drawing.Point(11, 236);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(74, 53);
+            this.button1.Size = new System.Drawing.Size(56, 43);
             this.button1.TabIndex = 16;
             this.button1.Text = "Infos";
             this.button1.UseVisualStyleBackColor = true;
@@ -175,9 +184,11 @@
             this.gBsal.Controls.Add(this.txtsal);
             this.gBsal.Controls.Add(this.txtnums);
             this.gBsal.Controls.Add(this.txtnoms);
-            this.gBsal.Location = new System.Drawing.Point(275, 209);
+            this.gBsal.Location = new System.Drawing.Point(206, 170);
+            this.gBsal.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.gBsal.Name = "gBsal";
-            this.gBsal.Size = new System.Drawing.Size(177, 124);
+            this.gBsal.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.gBsal.Size = new System.Drawing.Size(133, 101);
             this.gBsal.TabIndex = 18;
             this.gBsal.TabStop = false;
             this.gBsal.Text = "Ajout d\'un salarié";
@@ -185,49 +196,55 @@
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(6, 81);
+            this.label7.Location = new System.Drawing.Point(4, 66);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(60, 17);
+            this.label7.Size = new System.Drawing.Size(45, 13);
             this.label7.TabIndex = 19;
             this.label7.Text = "Salaire :";
             // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(6, 53);
+            this.label8.Location = new System.Drawing.Point(4, 43);
+            this.label8.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(45, 17);
+            this.label8.Size = new System.Drawing.Size(35, 13);
             this.label8.TabIndex = 18;
             this.label8.Text = "Nom :";
             // 
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(6, 27);
+            this.label9.Location = new System.Drawing.Point(4, 22);
+            this.label9.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(45, 17);
+            this.label9.Size = new System.Drawing.Size(35, 13);
             this.label9.TabIndex = 17;
             this.label9.Text = "Num :";
             // 
             // txtsal
             // 
-            this.txtsal.Location = new System.Drawing.Point(105, 78);
+            this.txtsal.Location = new System.Drawing.Point(79, 63);
+            this.txtsal.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtsal.Name = "txtsal";
-            this.txtsal.Size = new System.Drawing.Size(63, 22);
+            this.txtsal.Size = new System.Drawing.Size(48, 20);
             this.txtsal.TabIndex = 16;
             // 
             // txtnums
             // 
-            this.txtnums.Location = new System.Drawing.Point(57, 24);
+            this.txtnums.Location = new System.Drawing.Point(43, 20);
+            this.txtnums.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtnums.Name = "txtnums";
-            this.txtnums.Size = new System.Drawing.Size(111, 22);
+            this.txtnums.Size = new System.Drawing.Size(84, 20);
             this.txtnums.TabIndex = 15;
             // 
             // txtnoms
             // 
-            this.txtnoms.Location = new System.Drawing.Point(57, 50);
+            this.txtnoms.Location = new System.Drawing.Point(43, 41);
+            this.txtnoms.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtnoms.Name = "txtnoms";
-            this.txtnoms.Size = new System.Drawing.Size(111, 22);
+            this.txtnoms.Size = new System.Drawing.Size(84, 20);
             this.txtnoms.TabIndex = 14;
             // 
             // gBtemp
@@ -240,9 +257,11 @@
             this.gBtemp.Controls.Add(this.label4);
             this.gBtemp.Controls.Add(this.txtnump);
             this.gBtemp.Controls.Add(this.txtnomp);
-            this.gBtemp.Location = new System.Drawing.Point(272, 206);
+            this.gBtemp.Location = new System.Drawing.Point(204, 170);
+            this.gBtemp.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.gBtemp.Name = "gBtemp";
-            this.gBtemp.Size = new System.Drawing.Size(180, 144);
+            this.gBtemp.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.gBtemp.Size = new System.Drawing.Size(135, 114);
             this.gBtemp.TabIndex = 20;
             this.gBtemp.TabStop = false;
             this.gBtemp.Text = "Ajout d\'un temporaire";
@@ -250,74 +269,83 @@
             // 
             // txtnbh
             // 
-            this.txtnbh.Location = new System.Drawing.Point(92, 104);
+            this.txtnbh.Location = new System.Drawing.Point(69, 84);
+            this.txtnbh.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtnbh.Name = "txtnbh";
-            this.txtnbh.Size = new System.Drawing.Size(76, 22);
+            this.txtnbh.Size = new System.Drawing.Size(58, 20);
             this.txtnbh.TabIndex = 26;
             // 
             // txttaux
             // 
-            this.txttaux.Location = new System.Drawing.Point(57, 78);
+            this.txttaux.Location = new System.Drawing.Point(43, 63);
+            this.txttaux.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txttaux.Name = "txttaux";
-            this.txttaux.Size = new System.Drawing.Size(111, 22);
+            this.txttaux.Size = new System.Drawing.Size(84, 20);
             this.txttaux.TabIndex = 25;
             // 
             // label10
             // 
             this.label10.AutoSize = true;
-            this.label10.Location = new System.Drawing.Point(6, 105);
+            this.label10.Location = new System.Drawing.Point(4, 85);
+            this.label10.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(80, 17);
+            this.label10.Size = new System.Drawing.Size(61, 13);
             this.label10.TabIndex = 24;
             this.label10.Text = "NbHeures :";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(6, 81);
+            this.label5.Location = new System.Drawing.Point(4, 66);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(47, 17);
+            this.label5.Size = new System.Drawing.Size(37, 13);
             this.label5.TabIndex = 23;
             this.label5.Text = "Taux :";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(6, 53);
+            this.label3.Location = new System.Drawing.Point(4, 43);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(45, 17);
+            this.label3.Size = new System.Drawing.Size(35, 13);
             this.label3.TabIndex = 22;
             this.label3.Text = "Nom :";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(6, 27);
+            this.label4.Location = new System.Drawing.Point(4, 22);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(45, 17);
+            this.label4.Size = new System.Drawing.Size(35, 13);
             this.label4.TabIndex = 21;
             this.label4.Text = "Num :";
             // 
             // txtnump
             // 
-            this.txtnump.Location = new System.Drawing.Point(57, 24);
+            this.txtnump.Location = new System.Drawing.Point(43, 20);
+            this.txtnump.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtnump.Name = "txtnump";
-            this.txtnump.Size = new System.Drawing.Size(111, 22);
+            this.txtnump.Size = new System.Drawing.Size(84, 20);
             this.txtnump.TabIndex = 20;
             // 
             // txtnomp
             // 
-            this.txtnomp.Location = new System.Drawing.Point(57, 50);
+            this.txtnomp.Location = new System.Drawing.Point(43, 41);
+            this.txtnomp.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtnomp.Name = "txtnomp";
-            this.txtnomp.Size = new System.Drawing.Size(111, 22);
+            this.txtnomp.Size = new System.Drawing.Size(84, 20);
             this.txtnomp.TabIndex = 19;
             // 
             // radioButton1
             // 
             this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(275, 182);
+            this.radioButton1.Location = new System.Drawing.Point(206, 148);
+            this.radioButton1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(73, 21);
+            this.radioButton1.Size = new System.Drawing.Size(57, 17);
             this.radioButton1.TabIndex = 21;
             this.radioButton1.TabStop = true;
             this.radioButton1.Text = "Salarié";
@@ -327,9 +355,10 @@
             // radioButton2
             // 
             this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(350, 182);
+            this.radioButton2.Location = new System.Drawing.Point(262, 148);
+            this.radioButton2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(102, 21);
+            this.radioButton2.Size = new System.Drawing.Size(78, 17);
             this.radioButton2.TabIndex = 22;
             this.radioButton2.TabStop = true;
             this.radioButton2.Text = "Temporaire";
@@ -339,26 +368,28 @@
             // label11
             // 
             this.label11.AutoSize = true;
-            this.label11.Location = new System.Drawing.Point(293, 162);
+            this.label11.Location = new System.Drawing.Point(220, 132);
+            this.label11.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(130, 17);
+            this.label11.Size = new System.Drawing.Size(97, 13);
             this.label11.TabIndex = 23;
             this.label11.Text = "Employé à ajouter :";
             // 
             // label12
             // 
             this.label12.AutoSize = true;
-            this.label12.Location = new System.Drawing.Point(282, 9);
+            this.label12.Location = new System.Drawing.Point(212, 7);
+            this.label12.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label12.Name = "label12";
-            this.label12.Size = new System.Drawing.Size(158, 17);
+            this.label12.Size = new System.Drawing.Size(118, 13);
             this.label12.TabIndex = 24;
             this.label12.Text = "Département à ajouter :";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(455, 351);
+            this.ClientSize = new System.Drawing.Size(341, 285);
             this.Controls.Add(this.label12);
             this.Controls.Add(this.label11);
             this.Controls.Add(this.radioButton2);
@@ -376,8 +407,10 @@
             this.Controls.Add(this.listBox2);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.gBsal);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
-            this.Text = "TP Entreprise";
+            this.Text = "TP Entreprise by William";
             this.gBsal.ResumeLayout(false);
             this.gBsal.PerformLayout();
             this.gBtemp.ResumeLayout(false);
--- TPentrepriseGraphique/Form1.cs
+++ TPentrepriseGraphique/Form1.cs
@@ -11,21 +11,23 @@ using System.Windows.Forms;
 namespace TPentrepriseGraphique
 {
     public partial class Form1 : Form
-    {
+    {        
         SortedList<String, Departement> dep; 
         public Form1()
         {
             InitializeComponent();
-            dep = new SortedList<String, Departement>();
+            dep = new SortedList<String, Departement>(); // On instancie une liste de département qui seront affichés dans l'ordre
         }
         private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
         {      
 
         }
+        // AFFICHAGE des SALARIE en fonction du département choisi
         private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
             afficherSal();
         }
+        // CREATION d'un DEPARTEMENT puis on l'affiche sur le listeBox grâce à la méthode afficherliste()
         private void btndep_Click(object sender, EventArgs e)
         {
             Departement d = new Departement(txtdep.Text);
@@ -33,32 +35,43 @@ namespace TPentrepriseGraphique
             afficherliste();
             txtdep.Text = "";            
         }
+        // Méthode permettant d'afficher les départements sur le listBox
         private void afficherliste()
         {
             listBox1.DataSource = dep.Values.ToArray();
             listBox1.DisplayMember = "Nom";
         }
+        // SUPPRESSION du DEPARTEMENT
         private void supprdep_Click(object sender, EventArgs e)
         {
-            dep.RemoveAt(listBox1.SelectedIndex);
-            // ou bien : " dep.Remove(((Departement)listBox1.SelectedValue).Nom); "
+            dep.RemoveAt(listBox1.SelectedIndex);  // ou bien : " dep.Remove(((Departement)listBox1.SelectedValue).Nom); "
             afficherliste();
         }
+        // Lorsque l'on clique sur le bouton AJOUTER
         private void btnaddsal_Click(object sender, EventArgs e)
         {
-            Salarie s = new Salarie(Convert.ToInt16(txtnums.Text), txtnoms.Text,Convert.ToDouble(txtsal.Text));
-            ((Departement)listBox1.SelectedValue).addSalarie(s);
+            // AJOUT SALARIE
+            Salarie s = new Salarie( Convert.ToInt16 ( txtnums.Text ), txtnoms.Text,Convert.ToDouble ( txtsal.Text ) );
+            ((Departement)listBox1.SelectedValue).addSalarie(s);           
             afficherSal();
             txtnums.Text = "";
             txtnoms.Text = "";
             txtsal.Text = "";
+            // AJOUT TEMPORAIRE
+            Employe emp = new Temporaire( Convert.ToInt16 ( txtnump.Text ), txtnomp.Text, Convert.ToDouble( txttaux.Text ), Convert.ToInt16( txtnbh.Text ) );
+            txtnump.Text = "";
+            txtnomp.Text = "";
+            txttaux.Text = "";
+            txtnbh.Text = "";
         }
+        // Affichage du salarié en fonction du Département choisi
         private void afficherSal()
         {
             listBox2.DataSource = null;
             listBox2.DataSource = ((Departement)listBox1.SelectedItem).afficheListe();
             listBox2.DisplayMember ="Nom";
         }
+        // SUPPRESSION du SALARIE selectioné du département concerné
         private void btnsupprsal_Click(object sender, EventArgs e)
         {
             Salarie s = (Salarie)listBox2.SelectedValue;
@@ -66,12 +79,13 @@ namespace TPentrepriseGraphique
             d.supprimSalarie(s);
             afficherSal();
         }
-
+        // OUVERTURE de la deuxième FORM
         private void button1_Click(object sender, EventArgs e)
         {
             Form2 f = new Form2(((Salarie)listBox2.SelectedValue).Num, ((Salarie)listBox2.SelectedValue).Nom, ((Salarie)listBox2.SelectedValue).calculRemuneration());            
             f.Show();
         }
+        // SELECTION DU RADIOBOUTON SALARIE
         private void radioButton1_CheckedChanged(object sender, EventArgs e)
         {
             if (radioButton1.Checked == true)
@@ -84,7 +98,7 @@ namespace TPentrepriseGraphique
                 gBsal.Visible = false;
             }
         }
-
+        // // SELECTION DU RADIOBOUTON TEMPORAIRE
         private void radioButton2_CheckedChanged(object sender, EventArgs e)
         {
             if (radioButton2.Checked == true)
